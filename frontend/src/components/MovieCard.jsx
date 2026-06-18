d46b4fa30662c6d390e1abac4eb26db39f65fef9
snycn <236900862+snycn@users.noreply.github.com>
2026-06-18 02:14:46 -0400

    style: update favorites button

--- frontend/src/components/MovieCard.jsx
+++ frontend/src/components/MovieCard.jsx
@@ -11,7 +11,7 @@ function MovieCard({ movie }) {
         <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} />
         <div className="movie-overlay">
           <button className="favorite-btn" onClick={onFavoriteClick}>
-            dis a button
+            ♡
           </button>
         </div>
       </div>
